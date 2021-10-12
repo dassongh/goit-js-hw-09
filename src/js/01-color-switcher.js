c6a3da4bf3247a1c3dbafe33f7c2937a5f6d1090
Oleksandr S <oleksandr.slv@gmail.com>
2021-10-12 15:18:37 +0300

    adds first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+}
+let intervalId = null;
+
+refs.stop.disabled = true;
+
+refs.start.addEventListener('click', () => {
+  intervalId = setInterval(changeBgColor, 1000);
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+})
+
+refs.stop.addEventListener('click', () => {
+  clearInterval(intervalId);
+  refs.stop.disabled = true;
+  refs.start.disabled = false;
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
 
