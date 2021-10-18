7954eb9022b17394255d6a5f7ff68d84aba99923
Oleksandr S <oleksandr.slv@gmail.com>
2021-10-18 17:01:42 +0300

    reafactoring 02-timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Timer from './class-timer';
 import "flatpickr/dist/flatpickr.min.css";
 
 const refs = {
@@ -9,8 +10,6 @@ const refs = {
   minutes: document.querySelector('[data-minutes]'),
   seconds: document.querySelector('[data-seconds]'),
 }
-
-let countDownDate = 0;
 
 refs.start.disabled = true;
 
@@ -22,53 +21,12 @@ const options = {
   onClose(selectedDates) {
     if (Date.now() >= selectedDates[0].getTime()) return Notify.failure('Please choose a date in the future', { width: '400px' });
     refs.start.disabled = false;
-    countDownDate = selectedDates[0].getTime();
+    timer.date = selectedDates[0].getTime();
     timer.init()
   },
 };
 
 const fp = flatpickr("#datetime-picker", options);
-
-class Timer {
-  constructor({ onTick }) {
-    this.onTick = onTick;
-  }
-  
-  init() {
-    const time = this.convertMs(countDownDate - Date.now());
-    this.onTick(time);
-  }
-  
-  start() {
-    const startTime = countDownDate;
-    
-    this.intervalId = setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = startTime - currentTime;
-      const time = this.convertMs(deltaTime);
-      
-      this.onTick(time);
-    }, 1000);
-  }
-  
-  convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-    
-    const days = this.pad(Math.floor(ms / day));
-    const hours = this.pad(Math.floor((ms % day) / hour));
-    const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
-    const seconds = this.pad(Math.floor((((ms % day) % hour) % minute) / second));
-    
-    return { days, hours, minutes, seconds };
-  }
-
-  pad(value) {
-    return String(value).padStart(2, '0');
-  }
-}
 
 const timer = new Timer({ onTick: updateClockFace });
 
